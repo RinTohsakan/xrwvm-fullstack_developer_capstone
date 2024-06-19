c25c1530e75eb0c3244141481f71aea10a37221d
RinTohsakan <sjlf2004@gmail.com>
2024-06-19 05:57:35 -0400

    a

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,11 +1,11 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
-
+admin.site.register(CarMake)
 # CarModelInline class
-
+admin.site.register(CarModel)
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
